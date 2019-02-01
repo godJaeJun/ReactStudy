a3a47c416cef00812f1231c7a5ce082408f22ce3
이재준 <asapz@naver.com>
2019-02-01 21:31:45 +0900

    Chapter 5

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import MyComponent from "./MyComponent";
+//import MyComponent from "./MyComponent";  1~3장
+//import EventPractice from "./EventPractice"; 4장
+//import ValidationSample from "./ValidationSample"; 5장
+import ScrollBox from "./ScrollBox";
 
 //항상 부모가 있어야 된다. jsx특징.
 //*Fragment컴포넌트는 div 같은 것으로 감싸지 않고 여러 요소를 렌더링하고 싶을때 사용.
 class App extends Component {
   render() {
       return(
-        <MyComponent name="이재준" age={22}/>
+        <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+        <button onClick={()=>this.scrollBox.scrollToBottom()}>
+        맨 밑으로
+        </button>
+        </div>
       );
   }
 }
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -3,12 +3,9 @@ import PropTypes from "prop-types";
 //rcc를 이용해서 초기코드를 간단하게 생성할 수 있다.
 class MyComponent extends Component{
     
-    constructor(props){
-        super(props);
-        this.state={
-            number:0
-        }   
-    };
+    state={
+        number:0
+    }
 
     static defaultProps={   //props가 없을 경우 default props설정.
         name: "기본이름"
@@ -25,6 +22,11 @@ class MyComponent extends Component{
                 <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
                 <p>저는 {this.props.age}살 입니다.</p>
                 <p>숫자 : {this.state.number}</p>
+                <button onClick={()=>{
+                    this.setState({
+                        number:this.state.number+1
+                    })
+                }}>PLUS</button>
             </div>
         );
     }
