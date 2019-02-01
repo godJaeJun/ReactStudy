69c71cf98e871c5989b25921f89972d650f6740a
이재준 <asapz@naver.com>
2019-02-01 16:51:54 +0900

    eject

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -2,6 +2,13 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 //rcc를 이용해서 초기코드를 간단하게 생성할 수 있다.
 class MyComponent extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state={
+            number:0
+        }   
+    };
 
     static defaultProps={   //props가 없을 경우 default props설정.
         name: "기본이름"
@@ -17,6 +24,7 @@ class MyComponent extends Component{
             <div>
                 <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
                 <p>저는 {this.props.age}살 입니다.</p>
+                <p>숫자 : {this.state.number}</p>
             </div>
         );
     }
